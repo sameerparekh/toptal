a5c2f8370d86cdcd628a80224f822d3e5a5dd801
Sameer Parekh Brenn <sameer@creativedestruction.com>
2015-06-02 10:20:58 -0600

    add user creation

--- calorie_tracker/serializers.py
+++ calorie_tracker/serializers.py
@@ -2,6 +2,7 @@ __author__ = 'sameer'
 
 from rest_framework import serializers
 from models import Person, Meal
+from django.contrib.auth.models import User
 
 class MealSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,6 +15,17 @@ class PersonSerializer(serializers.ModelSerializer):
     meals = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = Person
-        fields = ('id', 'username', 'meals')
+        fields = ('id', 'expected_calories', 'username', 'email', 'password', 'meals')
+        extra_kwargs = {'password': {'write_only': True} }
 
-    username = serializers.ReadOnlyField(source='user.username')
+    username = serializers.CharField(source='user.username', read_only=False)
+    email = serializers.CharField(source='user.email', read_only=False)
+    password = serializers.CharField(source='user.password', read_only=False)
+
+    def create(self, validated_data):
+        username = validated_data['user']['username']
+        password = validated_data['user']['password']
+        email = validated_data['user']['email']
+        user = User.objects.create_user(username, email, password)
+        user.save()
+        return Person.objects.create(user=user, expected_calories=validated_data['expected_calories'])
