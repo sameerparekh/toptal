2de6f8d5555abded35b508bab034077f55535fbc
Sameer Parekh Brenn <sameer@creativedestruction.com>
2015-06-03 11:29:04 -0600

    basic functionality is here

--- calorie_tracker/static/calorie_tracker/calorie_tracker.js
+++ calorie_tracker/static/calorie_tracker/calorie_tracker.js
@@ -1,74 +1,96 @@
 var csrftoken = $.cookie('csrftoken');
 
 var module = angular.module("calorieTracker", [])
-var authToken = null
+module.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
 
-module.controller("RegisterLoginController", ['$scope', '$rootScope', '$http',
+                event.preventDefault();
+            }
+        });
+    };
+});
+
+module.controller("MainController", ['$scope', '$rootScope', '$http',
     function ($scope, $rootScope, $http) {
-        var controller = this
-        this.registerUser = function registerUser() {
+        $scope.authToken = null
+        $scope.expectedCalories = null
+        $scope.meals = []
+        $scope.totalCalories = null
+
+        $scope.registerUser = function registerUser() {
             $http.post("/calorie_tracker/users/", {
-                "username": controller.username,
-                "password": controller.password
+                "username": $scope.username,
+                "password": $scope.password
             }, {
                 "headers": { "X-CSRFToken": csrftoken }
             }).success(function (data, status, headers, config) {
-                controller.loginUser()
+                $scope.loginUser()
             })
         }
-        this.loginUser = function loginUser() {
+        $scope.loginUser = function loginUser() {
             $http.post("/calorie_tracker/api-token-auth/", {
-                "username": controller.username,
-                "password": controller.password
+                "username": $scope.username,
+                "password": $scope.password
             }, {
                 "headers": { "X-CSRFToken": csrftoken }
             })
                 .success(function (data, status, headers, config) {
-                    authToken = data.token
+                    $scope.authToken = data.token
+                    $scope.getUser()
+                    $scope.getMeals()
                 })
         }
-    }])
-
-module.controller("MealController", ['$scope', '$rootScope', '$http',
-    function ($scope, $rootScope, $http) {
-        this.deleteMeal = function deleteMeal(id) {
-            $http.delete("/calorie_tracker/meals/" + id, { "headers": { "Authorization": "Token " + authToken}})
+        $scope.getUser = function getUser() {
+            $http.get("/calorie_tracker/users/", { "headers": {"Authorization": "Token " + $scope.authToken}})
                 .success(function (data, status, headers, config) {
-                    this.getMeals()
+                    $scope.expectedCalories = data.expected_calories
+                    $scope.userId = data.id
                 })
         }
-        this.newMeal = function newMeal() {
+        $scope.updateUser = function updateUser() {
+            $http.put("/calorie_tracker/users/" + $scope.userId, { "expected_calories": $scope.expectedCalories }, { "headers": {"Authorization": "Token " + $scope.authToken}})
+        }
+        $scope.deleteMeal = function deleteMeal(meal) {
+            $http.delete("/calorie_tracker/meals/" + meal.id, { "headers": { "Authorization": "Token " + $scope.authToken}})
+                .success(function (data, status, headers, config) {
+                    $scope.getMeals()
+                })
+        }
+        $scope.newMeal = function newMeal() {
             $http.post("/calorie_tracker/meals/", {
-                "text": this.text,
-                "calories": this.calories
+                "text": $scope.text,
+                "calories": $scope.calories
             }, { "headers": { "X-CSRFToken": csrftoken,
-                "Authorization": "Token " + authToken
+                "Authorization": "Token " + $scope.authToken
             }})
                 .success(function (data, status, headers, config) {
-                this.getMeals()
-            })
+                    $scope.getMeals()
+                })
         }
-        this.updateMeal = function updateMeal(id) {
-            $http.put("/calorie_tracker/meals/" + id, {
-                "text": this.meals[id].text,
-                "calories": this.meals[id].calories,
-                "date": this.meals[id].date,
-                "time": this.meals[id].time
-            }, { "headers": {"Authorization": "Token " + authToken}})
+        $scope.updateMeal = function updateMeal(meal) {
+            $http.put("/calorie_tracker/meals/" + meal.id, {
+                "text": meal.text,
+                "calories": meal.calories,
+                "date": meal.date,
+                "time": meal.time
+            }, { "headers": {"Authorization": "Token " + $scope.authToken}})
                 .success(function (data, status, headers, config) {
-                this.getMeals()
-            })
+                    $scope.getMeals()
+                })
         }
-        this.getMeals = function getMeals() {
-            $http.get("/calorie_tracker/meals/", { "headers": {"Authorization": "Token " + authToken}})
+        $scope.getMeals = function getMeals() {
+            $http.get("/calorie_tracker/meals/", { "headers": {"Authorization": "Token " + $scope.authToken}})
                 .success(function (data, status, headers, config) {
-                    this.meals = {}
-                    this.total_calories = 0
-                    for (meal in data) {
-                        this.meals[meal.id] = meal
-                        this.total_calories += meal.calories
+                    $scope.meals = data
+                    $scope.totalCalories = 0
+                    for (meal of data) {
+                        $scope.totalCalories += meal.calories
                     }
                 })
         }
-        this.getMeals()
     }])
