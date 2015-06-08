4fecc1b1c100a9580d05450d4f562f72d7b7d914
Sameer Parekh Brenn <sameer@creativedestruction.com>
2015-06-08 15:32:03 -0600

    add some comments

--- calorie_tracker/permissions.py
+++ calorie_tracker/permissions.py
@@ -5,7 +5,7 @@ from rest_framework import permissions
 
 class IsOwnerOrStaff(permissions.BasePermission):
     """
-    Custom permission to only allow owners of an object to edit it.
+    Custom permission to only allow owners of an object or staff to edit it.
     """
 
     def has_object_permission(self, request, view, obj):
@@ -13,6 +13,9 @@ class IsOwnerOrStaff(permissions.BasePermission):
         return request.user.is_staff or obj.person.user == request.user
 
 class CreateOnlyIfNotAuth(permissions.BasePermission):
+    """
+    If the user hasn't authenticated, we can only create objects
+    """
     def has_permission(self, request, view):
         if not request.user or not request.user.is_authenticated():
             return request.method == "POST"
@@ -20,6 +23,9 @@ class CreateOnlyIfNotAuth(permissions.BasePermission):
             return True
 
 class SuperuserOrSelf(permissions.BasePermission):
+    """
+    Only superuser and self can modify user's settings
+    """
     def has_object_permission(self, request, view, obj):
         return request.user.is_superuser or obj.user == request.user
 
